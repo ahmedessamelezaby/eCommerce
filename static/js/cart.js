42a649bc785cb92f9bf2873e8b2be325e37bf86a
Mahmoud Orabi <101055442+Mahmoud462@users.noreply.github.com>
2023-03-18 15:32:12 +0200

    modified

--- static/js/cart.js
+++ static/js/cart.js
@@ -8,12 +8,33 @@ for(var i = 0 ; i < updateBtns.length; i++){
 
     console.log('USER:' , user)
     if(user === 'AnonymousUser'){
-      console.log('User is not authenticated')
+      addCookieItem(productId,action)
     }else{
       updateUserOrder(productId,action)
     }
   })
 }
+
+function addCookieItem(productId,action){
+  console.log('Not logged in..')
+  if (action == 'add') {
+    if (cart[productId] == undefined){
+      cart[productId] = {'quantity':1}
+    }else{
+      cart[productId]['quantity'] +=1
+    }
+  }
+  
+  if (action == 'remove') {
+    cart[productId]['quantity'] -=1
+    if (cart[productId]['quantity'] <= 0) {
+      console.log('remove item')
+      delete cart[productId]
+    }
+  }
+  console.log('cart:',cart)
+}
+
 
 function updateUserOrder(productId , action) {
   console.log('User is authenticated, sending data...')
--- store/models.py
+++ store/models.py
@@ -12,7 +12,7 @@ class Customer(models.Model):
 
 class Product(models.Model):
   name=models.CharField(max_length=100,null=True)
-  price=models.FloatField()
+  price=models.DecimalField(max_digits=7,decimal_places=2)
   digital=models.BooleanField(default=False,null=True,blank=True)
   image =models.ImageField(upload_to='image',null=True,blank=True)
   def __str__(self) -> str:
